89cdf2e731dab32e104657d541f40e5a4f1399df
Marco Cusenza <marco.cusenza.me@gmail.com>
2021-12-17 02:49:04 +0100

    struttura

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <img src="" alt="" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/partials/variables.scss";
+
+header {
+  height: 100px;
+  background-color: $darker;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/partials/variables.scss";
+
+</style>
